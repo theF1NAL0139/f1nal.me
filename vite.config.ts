d9f0f945c2f9b00009e4f7fec56c2782d2d48a88
Ivan Morozov <167004778+theF1NAL0139@users.noreply.github.com>
2025-11-25 00:07:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/f1nal-me/', // <-- Важно: имя репозитория со слешами
+  base: '/f1nal.me/', // <-- Важно: имя репозитория со слешами
 })
