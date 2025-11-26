86cefc7fdbbdaabfc49a9a37fa14cdb6274ac079
F1NAL <shvirov.oleg@gmail.com>
2025-11-26 22:12:13 +0300

    Fix base url for GitHub Pages

--- src/IssuuViewer.tsx
+++ src/IssuuViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { 
   ChevronLeft, 
   ChevronRight, 
