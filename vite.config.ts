5f637c300a72cbc31b99c791afb2c22ac387a4cb
F1NAL <shvirov.oleg@gmail.com>
2025-12-06 22:35:56 +0300

    Fix video size and base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,26 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   
-  // Важно для Github Pages или простого хостинга, чтобы пути были относительными
   base: '/',
-  chunkSizeWarningLimit: 1600, // Чтобы не ругался на большие чанки JS
 
   resolve: {
     alias: {
-      // ПРИНУДИТЕЛЬНО указываем путь к единственной версии React.
-      // Это лечит ошибку "Invalid hook call", если есть дубликаты.
       react: path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
     },
   },
 
   optimizeDeps: {
-    // Помогает Vite быстрее обработать pdfjs-dist при первом запуске
     include: ['pdfjs-dist'], 
   },
 
   build: {
-    // PDF.js использует современные фичи (Top-level await), 
-    // поэтому лучше ставить esnext или es2022
+    chunkSizeWarningLimit: 1600, 
+
     target: 'esnext', 
     commonjsOptions: {
       include: [/pdfjs-dist/, /node_modules/],
-      transformMixedEsModules: true, // Дополнительная подстраховка для смешанных модулей
+      transformMixedEsModules: true,
     }
   }
 })
